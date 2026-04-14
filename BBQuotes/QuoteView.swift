8d342bebb123dad6dd24e49c67649c7e37764d16
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-04-14 18:39:50 +0500

    - More about geometry reader - AsyncImage

---
+++ BBQuotes/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  BBQuotes
+//
+//  Created by Muhammad on 14/04/26.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
